79c701ab23a1f3544b228bfce56719615c172b67
dylanjue <155842992+dylanjue@users.noreply.github.com>
2025-05-26 13:36:26 +0300

    Add files via upload
    The tax assignment that implemented functions to return the tax and net pay as well as establishing loan eligibility.

---
+++ Taxation System Function/Taxation System Function/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        calculatebtn = New Button()
+        SuspendLayout()
+        ' 
+        ' calculatebtn
+        ' 
+        calculatebtn.Location = New Point(190, 122)
+        calculatebtn.Name = "calculatebtn"
+        calculatebtn.Size = New Size(388, 179)
+        calculatebtn.TabIndex = 0
+        calculatebtn.Text = "Calculate Tax and Net Pay"
+        calculatebtn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(calculatebtn)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents calculatebtn As Button
+
+End Class
---
+++ Taxation System Function/Taxation System Function/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim netPay As Double
+    Private Sub calculatebtn_Click(sender As Object, e As EventArgs) Handles calculatebtn.Click
+        Dim grossPay As Double
+        Dim tax As Double
+        Dim eligibleForLoan As Double
+
+        grossPay = InputBox("Enter the Gross Pay: ")
+
+        tax = tax_calculate(grossPay)
+        netPay = netPay_Calculate(grossPay, tax)
+        eligibleForLoan = loan_eligibility(netPay)
+
+    End Sub
+
+    Private Function tax_calculate(ByVal grossPay As Double) As Double
+        Dim tax As Double
+        If grossPay >= 500000 Then
+            tax = grossPay * 0.4
+        ElseIf grossPay >= 300000 Then
+            tax = grossPay * 0.3
+        ElseIf grossPay >= 200000 Then
+            tax = grossPay * 0.2
+        ElseIf grossPay >= 100000 Then
+            tax = grossPay * 0.1
+        Else
+            tax = 0
+        End If
+
+        MessageBox.Show("The Tax Implemented is: " & tax)
+        Return tax
+    End Function
+
+    Private Function netPay_Calculate(ByVal grossPay As Double, ByVal tax As Double) As Double
+        Dim netPay As Integer
+
+        netPay = grossPay - tax
+        MessageBox.Show("The Net Pay is: " & netPay)
+        Return netPay
+    End Function
+
+    Private Function loan_eligibility(ByVal netPay) As Double
+        Dim loan_netPay = netPay
+        If loan_netPay >= 350000 Then
+            MessageBox.Show("Your net pay is " & netPay & " ,which makes you eligible for a loan.")
+        Else
+            MessageBox.Show("Your net pay is " & netPay & " ,which makes you ineligible for a loan.")
+        End If
+        Return loan_netPay
+    End Function
+End Class
