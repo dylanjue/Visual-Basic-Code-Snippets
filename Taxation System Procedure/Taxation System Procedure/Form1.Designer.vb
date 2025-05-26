c3337c9f485e2b0c70cb4b536ca72e78f2bd5af9
dylanjue <155842992+dylanjue@users.noreply.github.com>
2025-05-26 13:42:17 +0300

    Add files via upload
    The tax assignment that implements a procedure that handles tax and net-pay calculation.

---
+++ Taxation System Procedure/Taxation System Procedure/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        calculatebtn = New Button()
+        SuspendLayout()
+        ' 
+        ' calculatebtn
+        ' 
+        calculatebtn.Location = New Point(209, 124)
+        calculatebtn.Name = "calculatebtn"
+        calculatebtn.Size = New Size(336, 130)
+        calculatebtn.TabIndex = 0
+        calculatebtn.Text = "Calculate"
+        calculatebtn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(calculatebtn)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents calculatebtn As Button
+
+End Class
---
+++ Taxation System Procedure/Taxation System Procedure/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub calculatebtn_Click(sender As Object, e As EventArgs) Handles calculatebtn.Click
+        Dim grossPay As Double
+
+        grossPay = InputBox("Enter the gross pay: ")
+
+        taxAndnetPay_Calculate(grossPay)
+    End Sub
+
+    Private Sub taxAndnetPay_Calculate(ByVal grossPay As Double)
+        Dim tax As Double
+        If grossPay >= 500000 Then
+            tax = grossPay * 0.4
+        ElseIf grossPay >= 300000 Then
+            tax = grossPay * 0.3
+        ElseIf grossPay >= 200000 Then
+            tax = grossPay * 0.2
+        ElseIf grossPay >= 100000 Then
+            tax = grossPay * 0.1
+        Else
+            tax = 0
+        End If
+
+        Dim netPay As Double
+        netPay = grossPay - tax
+
+        MessageBox.Show("Tax Implemented: " & tax)
+        MessageBox.Show("Net Pay: " & netPay)
+    End Sub
+
+End Class
